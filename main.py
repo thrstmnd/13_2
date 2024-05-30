c8c28351ab809c98b321da116dbb744e99e72145
thrstmnd <160107385+thrstmnd@users.noreply.github.com>
2024-05-30 10:22:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,24 @@ import tkinter as tk
 from tkinter import messagebox
 import wikipedia
 
-def show_facts():
+root = Tk()
+def facts():
     city = entry.get()
-    try:
-        page = wikipedia.page(city)
-        fact1 = page.summary.split('\n')[0]
-        fact2 = page.summary.split('\n')[1]
-        messagebox.showinfo("Исторические факты - {}".format(city), "{}\n{}".format(fact1, fact2))
-    except wikipedia.exceptions.PageError:
-        messagebox.showinfo("Ошибка", "Информация о данном городе отсутствует в базе данных.")
-
-root = tk.Tk()
+    page = wikipedia.page(city)
+    fact1 = page.summary.split('\n')[0]
+    fact2 = page.summary.split('\n')[1]
+    messagebox.showinfo("Исторические факты - {}".format(city), "{}\n{}".format(fact1, fact2))
+    
 root.geometry("300x300")
 root.title("Города")
 
-label = tk.Label(root, text="Введите название города:")
+label = Label(root, text="Введите название города:")
 label.pack()
 
-entry = tk.Entry(root)
+entry = Entry(root)
 entry.pack()
 
-button = tk.Button(root, text="Найти факты", command=show_facts)
+button = Button(root, text="Найти факты", command=facts)
 button.pack()
 
 root.mainloop()
